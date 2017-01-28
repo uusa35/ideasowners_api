d5293baf1a0f32d08d73cc6b4afd58ea9fab599b
vagrant <vagrant@homestead>
2017-01-28 13:54:27 +0000

    updating the admin messages

--- app/Core/Traits/MobileTrait.php
+++ app/Core/Traits/MobileTrait.php
@@ -13,20 +13,16 @@ trait MobileTrait
             'app_id' => "6f395d86-9da8-4ce4-8e15-56212a017092",
             'included_segments' => ['All'],
             'data' => ["title" => "test title", 'body' => 'test body'],
-            'headings' => ['en' => ($this->authJWT()->can('isAdmin')) ? 'Admin Message :' : $post->title],
+            'headings' => ['en' => $post->title],
             'template_id' => '51c7c1bc-d53d-4a40-904e-6a9a2b7f2dfe',
-            'contents' => ['en' => ($this->authJWT()->can('isAdmin')) ? 'Push Notification has been successfully sent ..' : $post->body],
+            'contents' => ['en' => $post->body],
             "included_segments" => ["All"],
             'small_icon' => 'http://ideasowners.net/wp-content/uploads/2016/03/128x128-2.jpg',
             'large_icon' => 'http://ideasowners.net/wp-content/uploads/2016/03/128x128-2.jpg',
             'big_picture' => 'http://ideasowners.net/wp-content/uploads/2016/03/128x128-2.jpg',
-//            'buttons' => json_encode([["id" => "id1", "text" => "button1", "icon" => "ic_launcher"], ["id" => "id2", "text" => "button2", "icon" => "ic_launcher"]])
         ];
 
         $fields = json_encode($fields);
-//        print("\nJSON sent:\n");
-//        print($fields);
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
@@ -39,21 +35,6 @@ trait MobileTrait
 
         $response = curl_exec($ch);
         curl_close($ch);
-
-//        $return["allresponses"] = $response;
-//        $return = json_encode($return);
-//
-//        print("\n\nJSON received:\n");
-//        print($return);
-//        print("\n");
-//
-//	$response = sendMessage();
-//	$return["allresponses"] = $response;
-//	$return = json_encode( $return);
-//
-//  print("\n\nJSON received:\n");
-//	print($return);
-//  print("\n");
 
     }
 
